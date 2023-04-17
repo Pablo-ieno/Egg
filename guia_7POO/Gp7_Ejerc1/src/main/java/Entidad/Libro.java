bc510fef926bcd03beda5c3a56886d3a268671e9
Pablo ieno <pabloieno@gmail.com>
2023-04-17 19:20:40 -0300

    Commit_4

--- guia_7POO/Gp7_Ejerc1/src/main/java/Entidad/Libro.java
+++ guia_7POO/Gp7_Ejerc1/src/main/java/Entidad/Libro.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Entidades;
+package Entidad;
 
 import java.util.Scanner;
 
@@ -11,7 +11,9 @@ import java.util.Scanner;
  * @author iptal
  */
 public class Libro {
-    
+    Scanner leer = new Scanner(System.in);
+
+    //Atributos.-
     public int ISBN;
     public String titulo;
     public String autor;
@@ -23,65 +25,34 @@ public class Libro {
 
     // Constructor Parametrizado.-
     public Libro(int ISBN, String titulo, String autor, int numPaginas) {
+
         this.ISBN = ISBN;
         this.titulo = titulo;
         this.autor = autor;
-        this.numPaginas = numPaginas;
-    }
 
-    public int getISBN() {
-        return ISBN;
-    }
-
-    public void setISBN(int ISBN) {
-        this.ISBN = ISBN;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public int getNumPaginas() {
-        return numPaginas;
-    }
-
-    public void setNumPaginas(int numPaginas) {
         this.numPaginas = numPaginas;
     }
 
     //Creación del Método para cargar un libro.-
     public void llenarLibro(int ISBN, String titulo, String autor, int numPaginas) {
         
-        Scanner leer = new Scanner(System.in);
- 
+
         System.out.print("Ingrese el nro de ISBN del libro: ");
-        int ISBN = leer.nextInt();
-        
-        System.out.print("Ingrese el título del libro: ");
-        leer.nextLine();/*ingresar un leer.nextLine si antes se leyó 
+//        ISBN = leer.nextInt();
+
+        System.out.println("Ingrese el título del libro: ");
+        /*leer.nextLine();*//*ingresar un leer.nextLine si antes se leyó
         un leer.nextInt() o leer.nextFloat() URL de la explicación práctica:
         https://www.tutorialesprogramacionya.com/javaya/detalleconcepto.php?codigo=86&punto=&inicio=*/
-        String titulo = leer.nextLine();
-        
-        System.out.print("Ingrese el nombre del autor del libro: ");
-        String autor=leer.nextLine();
-        
-        System.out.print("Ingrese la cantidad de páginas del libro: ");
-        int numPaginas = leer.nextInt();
+//        this.titulo = leer.nextLine();
 
-        // Se usa el this para diferenciar variable de atributo, ya que tienen el mismo nombre.-
+        System.out.print("Ingrese el nombre del autor del libro: ");
+//        this.autor = leer.nextLine();
+
+        System.out.print("Ingrese la cantidad de páginas del libro: ");
+//        numPaginas = leer.nextInt();
+
+// Se usa el this para diferenciar variable de atributo, ya que tienen el mismo nombre.-
         this.ISBN = ISBN;
         this.titulo = titulo;
         this.autor = autor;
@@ -98,6 +69,3 @@ public class Libro {
 
     }
 }
-
-    
-
--- guia_7POO/Gp7_Ejerc1/src/main/java/ip/gp7_ejerc1/Gp7_Ejerc1.java
+++ guia_7POO/Gp7_Ejerc1/src/main/java/ip/gp7_ejerc1/Gp7_Ejerc1.java
@@ -1,9 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ip.gp7_ejerc1;
 
+import Entidad.Libro;
 import Entidades.Libro;
 import java.util.Scanner;
 
@@ -19,25 +19,16 @@ public class Gp7_Ejerc1 {
  y un constructor vacío. Crear un método para cargar un libro pidiendo los datos al usuario 
  y luego informar mediante otro método el número de ISBN, el título, el autor del libro 
  y el número de páginas.*/
- 
- 
- Libro l1 = new Libro();
- Libro l2 = new Libro();
- 
- 
-        
-        System.out.println();
-        
+
+        Libro l1 = new Libro();
+
+//        System.out.println();
+
         //Llamado del Método para cargar los datos del libro.-
         l1.llenarLibro(ISBN, titulo, autor, numPaginas);
-        
+
         //Llamado del Método para mostrar los datos cargados.-
-        l1.mostrarLibro();      
- 
-         l2.llenarLibro(ISBN, titulo, autor, numPaginas);
-        
-       
-        l2.mostrarLibro();      
- 
+        l1.mostrarLibro();
+
     }
 }
